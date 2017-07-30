4ca0c845b0ac818c9b7e356f83c85d37bd24adf1
Jaime Gallego <github@gallegojai.me>
2017-07-31 01:49:45 +0200

    Added new functions, does not compile yet because of Werror

--- stprocras-list/stprocras-list.c
+++ stprocras-list/stprocras-list.c
@@ -1,19 +1,26 @@
-#define BOOL int
 #define STD_BUF_SIZE 4096
 #define TITLE_SIZE 128 
-#define TRUE 1
-#define FALSE 0
 
 #define BASE_FORMAT_TO_MODIFY "%%4i%%2i%%2iT%%2i%%2i%%2iZ;%%%i[^\t\n]"
 #define TESTFILE_PATH "tests/test.pcr"
 
+// Dummy returns so that GCC does not complain & to keep track of these tests
+#define TEST_DUMMY_TRUE 1
+#define TEST_DUMMY_FALSE 0
+
+// Error codes
+#define HALT_FILE_UNREADABLE -1
+#define HALT_FILE_FP_INVALID -2
+#define HALT_INVALID_DATE -3
+
 #include <argp.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-struct Deadline 
+struct ParsedDeadline 
 {
 	int yr;
 	int mon;
@@ -26,19 +33,30 @@ struct Deadline
 	char title[TITLE_SIZE];
 };
 
-BOOL fileExistsAndIsReadable(char* filePathToCheck);
-BOOL fpIsValid(FILE* fp);
-void zero_buffer(struct Deadline* cur_p);
+struct DeadlineInRelativeTime
+{
+	int days;
+	char title[TITLE_SIZE];
+};
 
+bool dateIsValid(struct ParsedDeadline* cur_p);
+bool fileExistsAndIsReadable(char* filePathToCheck);
+bool fpIsValid(FILE* fp);
+void printLine(struct DeadlineInRelativeTime calculatedDeadline);
+void zeroBuffer(struct ParsedDeadline* cur_p);
+struct DeadlineInRelativeTime calculateDaysToDeadline(struct ParsedDeadline* cur_p);
 
-/* TODO Add int argc, char* argv[] when implementing inputs */
+// TODO Add int argc, char* argv[] when implementing inputs
 int main(void)
 {
-	char* csvFilePath = TESTFILE_PATH; /* FIXME Testing */
+	// FIXME Testing. Should change depending on --continue-after- ... flag
+	bool continueAfterDateError = true;
+
+	char* csvFilePath = TESTFILE_PATH; // FIXME Testing
 	if (!fileExistsAndIsReadable(csvFilePath))
 	{
 		fprintf(stderr, "File does not exist or is not readable!\n");
-		return -1;
+		return HALT_FILE_UNREADABLE;
 	}
 	
 	FILE* fp = fopen(csvFilePath, "r");
@@ -46,35 +64,50 @@ int main(void)
 	if (!fpIsValid(fp))
 	{
 		fprintf(stderr, "File stream is not valid!\n");
-		return -2;
+		return HALT_FILE_FP_INVALID;
 	}
 
 	size_t bufferSize = sizeof(char) * STD_BUF_SIZE;
 	char* buffer = malloc(bufferSize);
 
-	/* Check how much space is needed for format fed to scanf */
+	// Check how much space is needed for format fed to scanf
 	size_t const formatLength = (size_t) snprintf(NULL, 0,
 				BASE_FORMAT_TO_MODIFY, TITLE_SIZE);
 
-	/* Line format: ISO 8601 + title string */	
+	// Line format: ISO 8601 + title string	
 	char lineFormat[formatLength + 1];
 
 	/* Craft the format that will be used in scanf.
-	 * Will keep the title length correct even if the
-	 * constant is changed */
+	 * Will keep the title length correct even if the format
+	 * layout is changed */
 	sprintf(lineFormat, BASE_FORMAT_TO_MODIFY, TITLE_SIZE);
 
-	struct Deadline* cur_p = malloc(sizeof(struct Deadline));
+	struct ParsedDeadline* cur_p = malloc(sizeof(struct ParsedDeadline));
 
-	while(getline(&buffer, &bufferSize, fp) != -1) 
+	for(int lineCount = 1; getline(&buffer, &bufferSize, fp) != -1; ++lineCount) 
 	{
-		zero_buffer(cur_p);
+		zeroBuffer(cur_p);
 		sscanf(buffer, lineFormat,
 				&(cur_p->yr), &(cur_p->mon), &(cur_p->day),
 				&(cur_p->hr), &(cur_p->min), &(cur_p->sec),
 				cur_p->title);
 
-		/* TODO */
+		if (!dateIsValid(cur_p))
+		{
+			fprintf(stderr, "Date in line %i is invalid", lineCount);
+
+			if (continueAfterDateError)
+			{
+				continue;
+			}
+
+			else
+			{
+				return HALT_INVALID_DATE;
+			}
+		}
+
+		// TODO
 	}
 	
 	free(cur_p);
@@ -84,17 +117,31 @@ int main(void)
 	return 0;
 }
 
-BOOL fileExistsAndIsReadable(char* filePathToCheck)
+bool dateIsValid(struct ParsedDeadline* cur_p)
 {
-	return (access(filePathToCheck, R_OK) == 0) ? TRUE : FALSE;
+	// TODO
+	
+	return TEST_DUMMY_TRUE; // FIXME Testing
 }
 
-BOOL fpIsValid(FILE* fp)
+bool fileExistsAndIsReadable(char* filePathToCheck)
 {
-	return (fp != NULL) ? TRUE : FALSE;
+	return (access(filePathToCheck, R_OK) == 0) ? true : false;
 }
 
-void zero_buffer(struct Deadline* cur_p)
+bool fpIsValid(FILE* fp)
+{
+	return (fp != NULL) ? true : false;
+}
+
+void printLine(struct DeadlineInRelativeTime calculatedDeadline) 
+{
+	// TODO
+	
+	return;
+}
+
+void zeroBuffer(struct ParsedDeadline* cur_p)
 {
 	
 	for (int titleLength = strlen(cur_p->title), i = 0; i < titleLength; i++)
@@ -103,4 +150,14 @@ void zero_buffer(struct Deadline* cur_p)
 	}
 
 	return;
+}
+
+struct DeadlineInRelativeTime calculateDaysToDeadline(struct ParsedDeadline* cur_p)
+{
+	struct DeadlineInRelativeTime calculatedDeadline;
+
+	
+	// TODO
+	
+	return calculatedDeadline;
 }
