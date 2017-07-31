a168a2efa777b0b3caefc30f221b0b831e47be34
Jaime Gallego <github@gallegojai.me>
2017-07-31 09:03:34 +0200

    Fixed memory error by adding mystery null-terminator byte to malloc size; changed date & time fields to unsigned; completed date validation.

--- stprocras-list/stprocras-list.c
+++ stprocras-list/stprocras-list.c
@@ -1,6 +1,8 @@
 #define STD_BUF_SIZE 4096
 #define TITLE_SIZE 128 
 
+#define EPOCH_YEAR 1970
+
 #define BASE_FORMAT_TO_MODIFY "%%4i%%2i%%2iT%%2i%%2i%%2iZ;%%%i[^\t\n]"
 #define TESTFILE_PATH "tests/test.pcr"
 
@@ -24,13 +26,13 @@
 
 struct ParsedDeadline 
 {
-	int yr;
-	int mon;
-	int day;
+	unsigned int yr;
+	unsigned int mon;
+	unsigned int day;
 
-	int hr;
-	int min;
-	int sec;
+	unsigned int hr;
+	unsigned int min;
+	unsigned int sec;
 
 	char title[TITLE_SIZE];
 };
@@ -41,9 +43,14 @@ struct DeadlineInRelTime
 	char title[TITLE_SIZE];
 };
 
-bool dateIsValid(struct ParsedDeadline* prsd_p);
+bool dateIsValid(unsigned int yr, unsigned int mon, unsigned int day);
+bool datetimeIsValid(struct ParsedDeadline* prsd_p);
 bool fileExistsAndIsReadable(char* filePathToCheck);
 bool fpIsValid(FILE* fp);
+bool monthHasThirtyOneDays(unsigned int mon);
+bool monthIsFebruary(unsigned int mon);
+bool timeIsValid(unsigned int hr, unsigned int min, unsigned int sec);
+bool yearIsLeap(unsigned int yr);
 void printRelativeDeadline(struct DeadlineInRelTime rel);
 void zeroParsedDeadlineTitle(struct ParsedDeadline* prsd_p);
 struct DeadlineInRelTime generateRelDeadline(struct ParsedDeadline* prsd_p);
@@ -81,7 +88,7 @@ int main(void)
 	 * layout is changed */
 	sprintf(lineFormat, BASE_FORMAT_TO_MODIFY, TITLE_SIZE);
 
-	struct ParsedDeadline* prsd_p = malloc(sizeof(struct ParsedDeadline));
+	struct ParsedDeadline* prsd_p = malloc(sizeof(struct ParsedDeadline) + 1);
 
 	for (int lineCount = 1;
 			getline(&buffer, &bufferSize, fp) != -1; ++lineCount) 
@@ -92,9 +99,9 @@ int main(void)
 				&(prsd_p->hr), &(prsd_p->min), &(prsd_p->sec),
 				prsd_p->title);
 
-		if (!dateIsValid(prsd_p))
+		if (!datetimeIsValid(prsd_p))
 		{
-			fprintf(stderr, "Invalid date in line %i\n", lineCount);
+			fprintf(stderr, "Invalid datetime in line %i\n", lineCount);
 
 			if (CONTINUE_AFTER_DATE_ERROR)
 			{
@@ -117,21 +124,105 @@ int main(void)
 	return 0;
 }
 
-bool dateIsValid(struct ParsedDeadline* prsd_p)
+bool datetimeIsValid(struct ParsedDeadline* prsd_p)
 {
-	// TODO
+	return (dateIsValid(prsd_p->yr, prsd_p->mon, prsd_p->day) &&
+			timeIsValid(prsd_p->hr, prsd_p->min, prsd_p->sec));
+}
+
+bool dateIsValid(unsigned int yr, unsigned int mon, unsigned int day)
+{
+	// Year bounded by epoch
+	if (yr < EPOCH_YEAR)
+	{
+		return false;
+	}
+
+	else if (mon < 1 || mon > 12)
+	{
+		return false;
+	}
+
+	else if (monthIsFebruary(mon))
+	{
+		if (yearIsLeap(yr) && day <= 29)
+		{
+			return true;
+		}
+
+		else if (day <= 28)
+		{
+			return true;
+		}
+	}
+
+	else if (day >= 1 && day <= 30)
+	{
+		return true;
+	}
+
+	else if (day >= 1 && monthHasThirtyOneDays(mon) && day <= 31)
+	{
+		return true;
+	}
 	
-	return TEST_DUMMY_TRUE; // FIXME Testing
+	return false;
 }
 
 bool fileExistsAndIsReadable(char* filePathToCheck)
 {
-	return (access(filePathToCheck, R_OK) == 0) ? true : false;
+	return (access(filePathToCheck, R_OK) == 0);
 }
 
 bool fpIsValid(FILE* fp)
 {
-	return (fp != NULL) ? true : false;
+	return (fp != NULL);
+}
+
+bool monthHasThirtyOneDays(unsigned int mon)
+{
+	// "Thirty days hath September / April, June and November ..."	
+	// February should also return false
+	// That leaves Jan, Mar, May, Jul, Aug, Oct, Dec as true
+	
+	switch(mon)
+	{
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			return true;
+		default:
+			return false;
+	}
+}
+
+bool monthIsFebruary(unsigned int mon)
+{
+	return (mon == 2);
+}
+
+bool timeIsValid(unsigned int hr, unsigned int min, unsigned int sec)
+{
+	return (hr <= 23 && min <= 59 && sec <= 59);
+}
+
+bool yearIsLeap(unsigned int yr)
+{
+	if (yr % 4 == 0)
+	{
+		if (!(yr % 400 == 0) && (yr % 100 == 0))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	return false;
 }
 
 void printRelativeDeadline(struct DeadlineInRelTime rel) 
@@ -143,9 +234,7 @@ void printRelativeDeadline(struct DeadlineInRelTime rel)
 
 void zeroParsedDeadlineTitle(struct ParsedDeadline* prsd_p)
 {
-	
-	for (int titleLength = strlen(prsd_p->title), i = 0;
-			i < titleLength; i++)
+	for (int i = 0;	i < TITLE_SIZE + 1; i++)
 	{
 		prsd_p->title[i] = '\0';
 	}
